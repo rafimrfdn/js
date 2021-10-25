0a8484b4b2b9ee9da20ad916915cc85f7ea84f4d
Muhammad Rafiuddin <71545866+rafimrfdn@users.noreply.github.com>
2021-10-25 08:12:18 +0800

    Update one-page-scroll.js

--- one-page-scroll.js
+++ one-page-scroll.js
@@ -1,80 +1,80 @@
 const TIME_OUT = 1200 // It should be the same transition time of the sections
-const body = document.querySelector('body')
-const layout = document.querySelector('.layout')
-const sectionsQty = document.querySelectorAll('section').length
-const sectionStick = document.querySelector('.section-stick')
+      const body = document.querySelector('body')
+      const layout = document.querySelector('.layout')
+      const sectionsQty = document.querySelectorAll('section').length
+      const sectionStick = document.querySelector('.section-stick')
 
-let startFlag = true
-let initialScroll = window.scrollY
-let qty = 1,
-    main = null,
-    next = null
+      let startFlag = true
+      let initialScroll = window.scrollY
+      let qty = 1,
+        main = null,
+        next = null
 
-let end, footer
+      let end, footer
 
-// Add child elements in .section-stick as number of sections exist
-Array(sectionsQty)
-    .fill()
-    .forEach(() => {
-        sectionStick.innerHTML = sectionStick.innerHTML + '<div class="stick"></div>'
-    })
+      // Add child elements in .section-stick as number of sections exist
+      Array(sectionsQty)
+        .fill()
+        .forEach(() => {
+          sectionStick.innerHTML = sectionStick.innerHTML + '<div class="stick"></div>'
+        })
 
-console.log('SLIDE', qty)
+      console.log('SLIDE', qty)
 
-// Listening to scroll event
-window.onscroll = () => {
-    if (startFlag) {
-        const scrollDown = this.scrollY >= initialScroll
-        // TODO: crear una constante para sectionsQty + 1
-        const scrollLimit = qty >= 1 && qty <= sectionsQty + 1
+      // Listening to scroll event
+      window.onscroll = () => {
+        if (startFlag) {
+          const scrollDown = this.scrollY >= initialScroll
+          // TODO: crear una constante para sectionsQty + 1
+          const scrollLimit = qty >= 1 && qty <= sectionsQty + 1
 
-        // Verify that the scroll does not exceed the number of sections
-        if (scrollLimit) {
+          // Verify that the scroll does not exceed the number of sections
+          if (scrollLimit) {
             body.style.overflowY = 'hidden' // Lock el scroll
 
             if (scrollDown && qty < sectionsQty) {
-                main = document.querySelector(`section.s${qty}`)
-                next = document.querySelector(`section.s${qty + 1}`)
+              main = document.querySelector(`section.s${qty}`)
+              next = document.querySelector(`section.s${qty + 1}`)
 
-                main.style.top = '-100vh'
-                next.style.top = '0'
+              main.style.top = '-100vh'
+              next.style.top = '0'
 
-                qty++
+              qty++
             } else if (!scrollDown && qty > 1) {
-                if (qty !== sectionsQty + 1) {
-                    main = document.querySelector(`section.s${qty - 1}`)
-                    next = document.querySelector(`section.s${qty}`)
+              if (qty !== sectionsQty + 1) {
+                main = document.querySelector(`section.s${qty - 1}`)
+                next = document.querySelector(`section.s${qty}`)
 
-                    main.style.top = '0'
-                    next.style.top = '100vh'
-                } else body.classList.remove('end')
+                main.style.top = '0'
+                next.style.top = '100vh'
+              } else body.classList.remove('end')
 
-                qty--
+              qty--
             } else {
-                // Mark the end of the slider on the body
-                body.classList.add('end')
-                qty++
+              // Mark the end of the slider on the body
+              body.classList.add('end')
+              qty++
             }
 
             // Scroll progressbar
             if (qty !== sectionsQty + 1) {
-                const active = document.querySelector('.section-stick .stick.active')
-                active.style.top = (62 + 30) * (qty - 1) + 'px'
+              const active = document.querySelector('.section-stick .stick.active')
+              active.style.top = (62 + 30) * (qty - 1) + 'px'
             }
-        }
+          }
 
-        console.log('SLIDE', qty)
+          console.log('SLIDE', qty)
 
-        // Wait for the scrolling to finish to reset the values
-        setTimeout(() => {
+          // Wait for the scrolling to finish to reset the values
+          setTimeout(() => {
             initialScroll = this.scrollY
             startFlag = true
             body.style.overflowY = 'scroll' // Unlock scroll
-        }, TIME_OUT)
+          }, TIME_OUT)
 
-        startFlag = false
-    }
+          startFlag = false
+        }
 
-    // Keep scrollbar in the middle of the viewport
-    window.scroll(0, window.screen.height)
-}
+        // Keep scrollbar in the middle of the viewport
+        window.scroll(0, window.screen.height)
+      }
